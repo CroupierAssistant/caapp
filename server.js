b03dfdb9d08de5c897354ddc3737ee2079602b1b
G0rdonShumway <64680974+G0rdonShumway@users.noreply.github.com>
2023-10-26 22:51:37 +0400

    123

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const db = require("./db");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const fs = require("fs");
+const path = require('path');
 
 const multer  = require('multer');
 
