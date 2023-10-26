90493dd2ec08b436184645cbed5469d400a52ae1
CroupierAssistant <133050570+CroupierAssistant@users.noreply.github.com>
2023-10-26 11:32:38 +0000

    1

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const jwt = require("jsonwebtoken");
 const fs = require("fs");
 
 const {User, findUserById} = require("./models/User");
+const User = require("./models/User");
 
 const BlackjackResult = require("./models/BlackjackResult"); // Import the TestResult model
 const MultiplicationResult = require("./models/MultiplicationResult"); // Import the TestResult model
